9be5261a01ac8b6e9bbb6f32c693a0c11b3463fe
Gergely Imreh <imrehg@gmail.com>
2018-05-25 12:38:47 +0100

    automatically get the tor user's user id for robustness

--- torproxy/start.sh
+++ torproxy/start.sh
@@ -1,10 +1,14 @@
 #!bin/bash
 
+# We are adding an iptables exception for the tor user, so while everything
+# else is proxied, processes run by the tor user are not
+TOR_UID=$(id --user tor)
+
 # Add rule
-if iptables -t nat -C OUTPUT -p tcp -m owner --uid-owner 101 -j RETURN ; then
+if iptables -t nat -C OUTPUT -p tcp -m owner --uid-owner ${TOR_UID} -j RETURN ; then
     echo "iptables exit rule for tor already set"
 else
-    iptables -t nat -I OUTPUT -p tcp -m owner --uid-owner 101 -j RETURN
+    iptables -t nat -I OUTPUT -p tcp -m owner --uid-owner ${TOR_UID} -j RETURN
 fi
 
 tail -f /var/log/tor/notices.log
